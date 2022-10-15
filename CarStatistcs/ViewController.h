c514b1e72d944f1b62126ab33b7f22386242193e
Andre Rodrigues <andrecomerciale@gmail.com>
2022-10-14 22:02:02 -0300

    feat: created random cars list

--- CarStatistcs/ViewController.h
+++ CarStatistcs/ViewController.h
@@ -9,6 +9,17 @@
 
 @interface ViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+
+@property (weak, nonatomic) IBOutlet UILabel *label1;
+@property (weak, nonatomic) IBOutlet UILabel *label2;
+@property (weak, nonatomic) IBOutlet UILabel *label3;
+@property (weak, nonatomic) IBOutlet UILabel *label4;
+@property (weak, nonatomic) IBOutlet UILabel *label5;
+@property (weak, nonatomic) IBOutlet UILabel *label6;
+
+- (IBAction)randomCar:(id)sender;
+
 
 @end
 
--- CarStatistcs/ViewController.m
+++ CarStatistcs/ViewController.m
@@ -18,5 +18,22 @@
     // Do any additional setup after loading the view.
 }
 
-
+- (IBAction)randomCar:(id)sender {
+    
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"RandomCars" ofType:@"plist"];
+    
+    NSDictionary *dict = [[NSDictionary alloc] initWithContentsOfFile:path];
+    NSMutableArray *array = dict[@"Cars"];
+    
+    int randomNum = arc4random() % [array count];
+    NSDictionary *dataSelect = array[randomNum];
+    
+    self.imageView.image = [UIImage imageNamed:dataSelect[@"Image"]];
+    self.label1.text = dataSelect[@"Name"];
+    self.label2.text = [NSString stringWithFormat: @"Top Speed: %@", dataSelect[@"Top Speed"]];
+    self.label3.text = [NSString stringWithFormat: @"0-60: %@", dataSelect[@"0-60"]];
+    self.label4.text = [NSString stringWithFormat: @"BHP: %@", dataSelect[@"BHP"]];
+    self.label5.text = [NSString stringWithFormat: @"MPG: %@", dataSelect[@"MPG"]];
+    self.label6.text = [NSString stringWithFormat: @"Price: %@", dataSelect[@"Price"]];
+}
 @end
